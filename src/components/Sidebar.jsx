17649fee092e786317d6388932dc28b465a8bd11
Pedro Germano <pedrogermano232@gmail.com>
2024-05-18 16:02:36 -0300

    FEAT: sidebar component done

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,3 +1,4 @@
+import { PencilLine } from "phosphor-react";
 import styled from "./Sidebar.module.css";
 
 export function Sidebar() {
@@ -18,7 +19,10 @@ export function Sidebar() {
         <span>Front-end Developer</span>
       </div>
       <footer>
-        <a href="#">Editar seu perfil</a>
+        <a href="#">
+          <PencilLine size={20} />
+          Editar seu perfil
+        </a>
       </footer>
     </aside>
   );
