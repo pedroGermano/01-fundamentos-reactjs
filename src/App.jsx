64fc64291c93927e91abdf76753c22706b1fa244
Pedro Germano <pedrogermano232@gmail.com>
2024-05-18 15:03:25 -0300

    FEAT: component sidebar create and add

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,23 @@ import { Header } from "./components/Header";
 import { Post } from "./Post";
 
 import "./global.css";
+import styled from "./App.module.css";
 
 export function App() {
   return (
     <div>
       <Header />
-      <Post
-        author="Pedro Germano"
-        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi 
-        nostrum quam temporibus nesciunt totam, optio sint, quos consequuntur quisquam ducimus a, 
-        doloremque deleniti omnis. Distinctio incidunt magni est quas beatae?"
-      />
-      <Post author="Germano" content="teste teste" />
+      <div className={styled.wrapper}>
+        <aside>Sidebar</aside>
+        <main>
+          <Post
+            author="Pedro Germano"
+            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Odit reprehenderit velit perferendis aperiam ullam, eveniet facere
+            qui minima nihil pariatur ut maiores at repudiandae, earum provident quis placeat libero minus!"
+          />
+        </main>
+      </div>
     </div>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,10 @@
 import styled from "./Header.module.css";
+import igniteLogo from "../assets/ignite-logo.svg";
 
 export function Header() {
   return (
     <header className={styled.header}>
+      <img src={igniteLogo} alt="Logo ignite feed" />
       <strong>Ignite Feed</strong>
     </header>
   );
